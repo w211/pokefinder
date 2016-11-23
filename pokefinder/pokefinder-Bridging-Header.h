8f9e6f629aad3b7951eb8c257c9c3990cfb50dfa
w211 <rcuico@gmail.com>
2016-11-23 13:38:05 -0800

    added mapkit

---
+++ pokefinder/pokefinder-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  pokefinder-Bridging-Header.h
+//  pokefinder
+//
+//  Created by Richard Cuico on 11/23/16.
+//  Copyright © 2016 Richard Cuico. All rights reserved.
+//
+
+#import "GeoFire.h"
